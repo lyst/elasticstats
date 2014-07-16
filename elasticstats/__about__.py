115fccfbe8c337f0690aa0b05153a47be1583d03
Igor Serko <igor.serko@gmail.com>
2014-07-16 12:42:06 +0100

    added setup.py

---
+++ elasticstats/__about__.py
@@ -0,0 +1,19 @@
+__all__ = [
+    "__title__", "__summary__", "__uri__", "__version__", "__author__",
+    "__email__", "__license__", "__copyright__",
+]
+
+__title__ = "elasticstats"
+__summary__ = (
+    "elasticstats populates a Graphite server with stats from Elasticsearch"
+)
+__uri__ = "https://github.com/ssaw/elasticstats"
+
+__version__ = "0.1.0"
+
+__author__ = "LYST LTD"
+__email__ = "info@lyst.com"
+
+__license__ = "LGPLv3"
+__copyright__ = "Copyright 2013-2014 %s" % __author__
+
